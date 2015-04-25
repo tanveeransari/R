8164b50d6e9889f7c9f6f3fec9d3022dd36c7064
Tanveer.Ansari <tanveeransari@hotmail.com>
2015-04-25 02:11:25 -0500

    Initial Draft regression models project

--- Regression/Project.R
+++ Regression/Project.R
@@ -11,31 +11,30 @@
 # 3) Constant variability of residuals. Make plot of residuals vs predicted (yhat) [Not residuals vs x]
 # 4) Independent residuals . Just plot residuals without any x axis
 
+require("car");require("GGally")
+
 data(mtcars);
 #Maybe to use pairs for exploratory analysis and
-# to evaluate (mullti)COLLINEARITY b/w variables
-#require("GGally");
-pairs(mtcars)
-pairs(~mpg+disp+hp+qsec+am+wt,data=mtcars,main="Title")
-#ggpairs(mtcars,,lower=list(continous="smooth",params=c(method="loess")))
+#pairs(~mpg+disp+hp+qsec+am+wt,data=mtcars,main="Title")
+ggpairs(c(1,3,4,6,7,9),data=mtcars,lower=list(continous="smooth",params=c(method="loess")))
 
-# My strategy - first use SLM mpg vs am
-# Next use MultipleRegression, then compare multiple models
-# by combining various factors intelligently
 # The following yielded pretty good results
 # summary((lm(mpg~wt+hp+am+qsec,data=mtcars)))
 
-# To compare coeffecients or residuals or anything of various models use
-# lapply(list(m1,m2,m3),coef)
+all<-lm(mpg~.,mtcars)
+wthp<-lm(mpg~wt+hp,mtcars)
+wthpqsec<-lm(mpg~wt+hp+qsec,mtcars)
+wthpam<-lm(mpg~wt+hp+am,mtcars)
+wthpqsecam<-lm(mpg~wt+hp+qsec+am,mtcars)
+wthpqsecamdspl<-lm(mpg~wt+hp+qsec+am+disp,mtcars)
+models<-list("all"=all,"wthp"=wthp,"wthpqsec"=wthpqsec,"wthpam"=wthpam,
+             "wthpqsecam"=wthpqsecam,"wthpqsecamdspl"=wthpqsecamdspl)
+
+sapply(models,function(x){summary(x)$adj.r.squared})
+
+
+anova(wthp,wthpam,wthpqsecam,wthpqsecamdspl)
+anova(wthp,wthpqsec,wthpqsecam,wthpqsecamdspl)
 
 # Maybe generate C.I. of effect of am on mpg
 # See https://class.coursera.org/statistics-003/lecture/167
-require("VIF")
-# Maybe generate variance inflation of adding am to set of other variables
-# Manual way
-# a<-suumar(lm(y~factor1+factor2=...factor[n-1]))$cov.unscaled[2,2] #From variance inflation lecture
-# summary(lm(y~factor1+factor2+...+factor[n]))$cov.unscaled[2,2]/a # give us VIF of adding N
-# Automatic way is to the vif(fit) function
-
-# Finally use the F test value of am to test hypothesis that Beta[am] is non-zero
-# This value is given by the t stat and the P value is what we need to look at
