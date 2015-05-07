d4cd85885925dc0aff1c2fe2a1c5107dfb97e276
Tanveer.Ansari <tanveeransari@hotmail.com>
2015-05-06 22:55:25 -0500

    shiny.rstudio.com lesson 2

--- shiny/App-1/ui.R
+++ shiny/App-1/ui.R
@@ -1,24 +1,44 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
+  fluidPage
+  (
+    titlePanel("My Shiny App"),
 
-  # Application title
-  titlePanel("Hello World!"),
+    sidebarLayout
+    (
+      sidebarPanel
+      (
+        h2("Installation"),
+        p("Shiny is available on CRAN, so you can install it in the usual
+        way from your R console:"),
+        code("install.packages(\"shiny\")", style="color:red"),
+        br(),
+        span(
+          img(src="bigorb.png", height=72, width=72),
+          "shiny is a product of",
+          span("RStudio", style="color:blue")
+        )
+      ),
 
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 5,
-                  max = 50,
-                  value = 30)
-    ),
+      mainPanel(
+        h1("Intorducing Shiny"),
+        p("Shiny is a new package from RStudio that makes it",
+          em("incredibly easy"),
+          "to build interactive web applications with R."
+        ),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+        p("For an introduction and live examples, visit the",
+          a("Shiny Home Page", href="http://shiny.rstudio.com")
+        ),
+
+        h2("Features"),
+        p("* Build useful web applications with only a few lines of code -
+        no JavaScript required."),
+        p("* Shiny applications are automatically \"live\" in the sampe way that
+        spreadsheets are live. Outputs change instantly as users modify inputs,without requireing a reload of the browser")
+      )
     )
   )
-))
+)
