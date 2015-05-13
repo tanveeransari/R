b054124ca63180313938a1608db5f09199e6efc5
Tanveer Ansari <tanveeransari@hotmail.com>
2015-05-12 19:58:03 -0500

    practical m/c learning Quiz2

---
+++ practicalmachinelearning/Quiz2.R
@@ -0,0 +1,48 @@
+#Q1
+#Only worked with list = F
+#----------------------------------
+#Q2
+#Question is wrong
+#----------------------------------
+#Q3
+# Doing a summary after doing log shows -Inf
+#----------------------------------
+#Q4
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+#----------------------------------
+ilNames<-names(training)[grep("^IL",names(training))]
+#Way to do without caret package
+#smallTrain<-training[,ilNames]
+# prc<-prcomp(smallTrain, center = T, scale=T)
+# summary(prc)
+#Answer 7 - by looking at cumulative variance
+smallTrain<-training[,ilNames]
+# From https://tgmstat.wordpress.com/2013/11/28/computing-and-visualizing-pca-in-r/
+trans<-preProcess(smallTrain, method=c("pca"), thresh = 0.8)
+trans$rotation
+#rotation provides representation of the OLD variables in terms of the 
+# principal components. - shows PC1 to PC7 so 
+#Answer 7
+#----------------------------------
+
+#Q5
+
+set.seed(3433)
+data(AlzheimerDisease)
+ilNames<-names(predictors)[grep("^IL",names(predictors))]
+adData = data.frame(diagnosis, predictors[ilNames])set.seed(3433)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+mdlAll<-train(diagnosis~.,method="glm",data=training)
+confusionMatrix(testing$diagnosis, predict(mdlAll, newdata=testing))
+
+mdlPCA<-train(diagnosis~.,method="glm",preProcess="pca",data=training,trControl=trainControl(preProcOptions = list(thresh=0.8)))
+confusionMatrix(testing$diagnosis, predict(mdlPCA, newdata=testing))
